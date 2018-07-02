0fe8b7c29ecb17137dfd114798a77fa7dcee7e68
andreeduardo <31081319+andreeduardo@users.noreply.github.com>
2018-07-02 20:05:20 -0300

    commit

--- dev/node-str/src/routes/chatRoute.js
+++ dev/node-str/src/routes/chatRoute.js
@@ -4,6 +4,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/chatController');
 
+AIMLInterpreter = require('../../node_modules/aimlinterpreter');
+
+var aimlInterpreter = new AIMLInterpreter();
+aimlInterpreter.loadAIMLFilesIntoArray(['../node-str/src/aiml/base.aiml.xml']);
+
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "chatController",
@@ -11,7 +16,18 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', controller.post);
-router.put('/:id', controller.put);
+var callback = function(answer, wildCardArray, input){
+    return answer;   
+};
+
+const create = router.post('/', (req, res, next) => {
+    
+    if (aimlInterpreter.findAnswerInLoadedAIMLFiles(req.body.pergunta,  callback) == null) { 
+        res.json({resposta: "Infelizmente nao encontrei nenhuma resposta sobre: " + req.body.pergunta 
+    + ".\n Por gentileza, tente perguntar de outra maneira ou me pergunte sobre BOLETOS ou NOTAS."}); 
+     } else {
+        res.json({resposta: aimlInterpreter.findAnswerInLoadedAIMLFiles(req.body.pergunta,  callback)});
+     }
+});
 
 module.exports = router;    
